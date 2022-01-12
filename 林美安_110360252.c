c0af54bef0f247fc9a4b0009459701de5fcaa588
SinIvan <91317657+SinIvan@users.noreply.github.com>
2022-01-12 23:59:27 +0800

    Update 林美安_110360252.c

--- 林美安_110360252.c
+++ 林美安_110360252.c
@@ -1,27 +1,36 @@
-int main()
+int get24(int a, int b, int c, int d)
 {
-	int a, b, c, d;
-	int replay;
-
-	do
-	{
-		printf("Please input four integer (1 - 13)\n");
-	loop: scanf("%d %d %d %d", &a, &b, &c, &d);
-		if (a < 1 || a > 13 || b < 1 || b > 13 || c < 1 || c > 13 || d < 1 || d > 13)
-		{
-			printf("Input illege, PLease input again\n");
-			goto loop;
-		}
-
-		if (get24(a, b, c, d));
-		else
-			printf("Sorry, the four integer cannot be calculated to get 24\n");
-		getchar();
-
-		printf("Y to replay & 0 to end");
-		scanf("%d", &replay);
-		getchar();
-	} while (replay);
-
-	return 0;
+	int op1, op2, op3;
+	int flag = 0;
+	for (op1 = 1; op1 <= 4; op1++)
+		for (op2 = 1; op2 <= 4; op2++)
+			for (op3 = 1; op3 <= 4; op3++)
+			{
+				if (calculate_mode11(a, b, c, d, op1, op2, op3) == 24)
+				{
+					printf("((%d%c%d)%c%d)%c%d=24\n", a, op[op1], b, op[op2], c, op[op3], d);
+					flag = 1;
+				}
+				if (calculate_mode12(a, b, c, d, op1, op2, op3) == 24)
+				{
+					printf("%d%c(%d%c%d)%c%d=24\n", a, op[op1], b, op[op2], c, op[op3], d);
+					flag = 1;
+				}
+				if (calculate_mode13(a, b, c, d, op1, op2, op3) == 24)
+				{
+					printf("%d%c(%d%c(%d%c%d))=24\n", a, op[op1], b, op[op2], c, op[op3], d);
+					flag = 1;
+				}
+				if (calculate_mode14(a, b, c, d, op1, op2, op3) == 24)
+				{
+					printf("%d%c((%d%c%d)%c%d)=24\n", a, op[op1], b, op[op2], c, op[op3], d);
+					flag = 1;
+				}
+				if (calculate_mode15(a, b, c, d, op1, op2, op3) == 24)
+				{
+					printf("(%d%c%d)%c(%d%c%d)=24\n", a, op[op1], b, op[op2], c, op[op3], d);
+					flag = 1;
+				}
+			}
+	return flag;
 }
